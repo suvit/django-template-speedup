69b6a910661d3601b806de2a601c09772b501348
Victor Safronovich <vsafronovich@gmail.com>
2013-12-12 19:51:40 +0600

    save templates to django cache

--- template_speedup/loader.py
+++ template_speedup/loader.py
@@ -1,3 +1,30 @@
 
+from django.template.loaders.cached import Loader as CachedLoader
 
-# memcached loader
+
+class DictToCache(object):
+    def __init__(self):
+        self.keys = set()
+
+    def __contains__(self, key):
+        return cache.get(key) is not None
+
+    def __getitem__(self, key):
+        return cache.get(key)
+
+    def __setitem__(self, key, value):
+        cache.set(key, value, 60 * 60 * 24 * 30) # one month
+        self.keys.add(key)
+
+    def clear(self):
+        cache.delete_many(self.keys)
+        self.keys.clear()
+
+
+class DjangoCacheLoader(CachedLoader):
+    # django cache loader
+    def __init__(self, loaders):
+        super(DjangoCacheLoader, self).__init__(loaders)
+
+        # override template_cache_var
+        self.template_cache = DictToCache()
