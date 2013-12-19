d04961527e473dfa5f615b5d9384cf1df87d300c
Victor Safronovich <vsafronovich@gmail.com>
2013-12-19 19:47:17 +0600

    more correct node rendering to file

--- template_speedup/render.py
+++ template_speedup/render.py
@@ -4,8 +4,9 @@ import functools
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, make_option
-from django.template.base import Node
+from django.template.base import Node, NodeList, VariableDoesNotExist
 from django.template.loader import get_template
+from django.template.defaulttags import IfNode, ForNode, WithNode
 from django.template.context import Context
 
 from django.utils.encoding import force_unicode
@@ -13,19 +14,113 @@ from django.utils.encoding import force_unicode
 logger = logging.getLogger(__name__)
 
 
-def patch_template(template, out_file):
-    def file_render(self, out_file, context):
-        bits = out_file
+class FileNodeList(object):
+    def __init__(self, nodelist, outfile):
+        self.nodelist = nodelist
+        self.outfile = outfile
+
+    def __iter__(self):
+        return iter(self.nodelist)
+
+    def render_for(nodelist, self, context):
+        if 'forloop' in context:
+            parentloop = context['forloop']
+        else:
+            parentloop = {}
+        context.push()
+        try:
+            values = self.sequence.resolve(context, True)
+        except VariableDoesNotExist:
+            values = []
+        if values is None:
+            values = []
+        if not hasattr(values, '__len__'):
+            values = list(values)
+        len_values = len(values)
+        if len_values < 1:
+            context.pop()
+            return self.nodelist_empty.render(context)
+        if self.is_reversed:
+            values = reversed(values)
+        unpack = len(self.loopvars) > 1
+        # Create a forloop value in the context.  We'll update counters on each
+        # iteration just below.
+        loop_dict = context['forloop'] = {'parentloop': parentloop}
+        for i, item in enumerate(values):
+            # Shortcuts for current loop iteration number.
+            loop_dict['counter0'] = i
+            loop_dict['counter'] = i+1
+            # Reverse counter iteration numbers.
+            loop_dict['revcounter'] = len_values - i
+            loop_dict['revcounter0'] = len_values - i - 1
+            # Boolean values designating first and last times through loop.
+            loop_dict['first'] = (i == 0)
+            loop_dict['last'] = (i == len_values - 1)
+
+            pop_context = False
+            if unpack:
+                # If there are multiple loop variables, unpack the item into
+                # them.
+                try:
+                    unpacked_vars = dict(zip(self.loopvars, item))
+                except TypeError:
+                    pass
+                else:
+                    pop_context = True
+                    context.update(unpacked_vars)
+            else:
+                context[self.loopvars[0]] = item
+            # In TEMPLATE_DEBUG mode provide source of the node which
+            # actually raised the exception
+            if settings.TEMPLATE_DEBUG:
+                for node in self.nodelist_loop:
+                    try:
+                        FileNodeList(node, nodelist.outfile).render(context)
+                    except Exception, e:
+                        if not hasattr(e, 'django_template_source'):
+                            e.django_template_source = node.source
+                        raise
+            else:
+                for node in self.nodelist_loop:
+                    FileNodeList(node, nodelist.outfile).render(context)
+            if pop_context:
+                # The loop variables were pushed on to the context so pop them
+                # off again. This is necessary because the tag lets the length
+                # of loopvars differ to the length of each set of items and we
+                # don't want to leave any vars from the previous loop on the
+                # context.
+                context.pop()
+        context.pop()
+        return u''
+
+    def render_node(self, node, context):
+        if isinstance(node, IfNode):
+            new_cn = []
+            for _, nodelist in node.conditions_nodelists:
+                new_cn.append((_, FileNodeList(nodelist, self.outfile)))
+            node.conditions_nodelists = new_cn
+        elif isinstance(node, ForNode):
+            return self.render_for(node, context)
+        elif isinstance(node, WithNode):
+            node.nodelist = FileNodeList(node.nodelist, self.outfile)
+
+        return node.render(context)
+
+    def render(self, context):
         for node in self:
+            print '--->', `node`
             if isinstance(node, Node):
                 bit = self.render_node(node, context)
             else:
                 bit = node
             bit = force_unicode(bit)
-            bits.write(bit.encode('utf-8'))
-        return 'rendered to file %s' % out_file
+            self.outfile.write(bit.encode('utf-8'))
+        return ''
 
-    template.nodelist.render = functools.partial(file_render, template.nodelist, out_file)
+
+def patch_template(template, out_file):
+
+    template.nodelist = FileNodeList(template.nodelist, out_file)
     return template
 
 
@@ -33,12 +128,11 @@ def render_to_file(out_file, template_name, context=None):
     # chuncked render to file
     template = get_template(template_name)
 
-    patched_template = patch_template(template, out_file)
-
     if context is None:
-        context=Context()
+        context = Context()
 
+    patched_template = patch_template(template, out_file)
     try:
-        template.render(context=context)
+        patched_template.render(context=context)
     finally:
-        del template.nodelist.render
+        del patched_template.nodelist
